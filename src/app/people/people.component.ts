a8553ddef163638d9643d5b2c3eb22282ef4e088
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-13 13:08:09 +0100

    Add people component with non-functional buttons

---
+++ src/app/people/people.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrl: './people.component.css'
+})
+export class PeopleComponent {
+  @Input() people: string[] = [];
+
+}
